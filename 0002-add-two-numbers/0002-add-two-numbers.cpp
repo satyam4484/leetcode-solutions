7dd93cb28a8364cb1a788eb58e80e2e49a9680e8
Satyam Singh <65668047+satyam4484@users.noreply.github.com>
2024-07-10 09:10:52 +0530

    Time: 32 ms (10.55%), Space: 76.1 MB (28.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,28 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode*head=nullptr,*tail=nullptr;
-        head = tail;
+        ListNode*head = new ListNode(-1);
+        ListNode*tail = head;
         int r = 0;
-        while(l1 or l2){
-            int a = l1?l1->val:0;
-            int b = l2?l2->val:0;
-            l1 = (l1?l1->next:nullptr);
-            l2=(l2?l2->next:nullptr);
-            int val = (r+a+b)%10;
-            ListNode*node = new ListNode(val);
-            r = (r+a+b)/10;
-            if(head == nullptr) {
-                head = node;
-                tail=node;
-            }else{
-                tail->next = node;
-                tail= node;
-            }
+        while(l1 or l2) {
+            int sum = (l1?l1->val:0) + (l2?l2->val:0) + r;
+            int val = sum%10;
+            r = sum/10;
+            tail->next = new ListNode(val);
+            tail = tail->next;
+            l1 = (l1? l1->next : nullptr);
+            l2 = (l2?l2->next : nullptr);
         }
-        if(r){
+
+        if(r > 0){
             tail->next = new ListNode(r);
+            tail = tail->next;
         }
-        return head;
+        return head->next;
     }
 };
