b0029ffcdcb7750d7ef79c69022b8b1ce1ebf581
Satyam Singh <65668047+satyam4484@users.noreply.github.com>
2026-01-06 09:17:37 +0530

    Time: 4 ms (57.77%), Space: 109.5 MB (30.13%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -6,43 +6,54 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     int maxLevelSum(TreeNode* root) {
-        unordered_map<int,int>mp;
-        queue<TreeNode*>q;
+        unordered_map<int, int> mp;
+        queue<TreeNode*> q;
         q.push(root);
         int level = 1;
-        while(!q.empty()) {
+        int mx = INT_MAX;
+        int max_ans = INT_MIN;
+        while (!q.empty()) {
             // iterate
             int sum = 0;
 
             int size = q.size();
-            for(int i=0;i<size;i++) {
+            for (int i = 0; i < size; i++) {
                 auto node = q.front();
                 q.pop();
-                sum+=node->val;
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+                sum += node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
             }
-            mp[level] = sum;
+
+            if (max_ans < sum) {
+                max_ans = sum;
+                mx = level;
+            } else if (max_ans == sum) {
+                mx = min(mx, level);
+            }
             level++;
         }
 
-        level = mp.size();
-        int max_ans = INT_MIN;
-        for(auto ele:mp) {
-            cout<<ele.first<<" "<<ele.second<<endl;
-            if(max_ans < ele.second) {
-                max_ans = ele.second;
-                level = ele.first;
-            }else if(max_ans == ele.second) {
-                level = min(level,ele.first);
-            }
-        }
-        return level;
+        // level = mp.size();
+
+        // for(auto ele:mp) {
+        //     cout<<ele.first<<" "<<ele.second<<endl;
+        //     if(max_ans < ele.second) {
+        //         max_ans = ele.second;
+        //         level = ele.first;
+        //     }else if(max_ans == ele.second) {
+        //         level = min(level,ele.first);
+        //     }
+        // }
+        return mx;
     }
 };
