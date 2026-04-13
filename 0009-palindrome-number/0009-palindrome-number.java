efb370e64c4f62de0416c5f9c42d4aa0659e2d83
Satyam Singh <65668047+satyam4484@users.noreply.github.com>
2026-04-13 09:31:53 +0530

    Time: 5 ms (79.63%), Space: 46.1 MB (22.54%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x< 0) return false;
-        int prev =x;
-        long curr = 0;
-        while(x!=0) {
-            curr = curr*10 + x%10;
-            x/=10;
+        if(x < 0 ) return false;
+        // approach could b we can convert to string and then check reversed approach.
+        // will cause some string space building.
+        int newValue = 0;
+        int preValue = x;
+        while(x>0) {
+            newValue = newValue*10 + x%10;
+            x = x/10;
         }
-        return curr == prev;
+        return newValue == preValue;
     }
 }
