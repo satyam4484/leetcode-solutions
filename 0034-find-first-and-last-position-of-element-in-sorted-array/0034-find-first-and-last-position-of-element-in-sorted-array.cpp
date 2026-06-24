c876d383e17395540b92f2cf2488a3b91d290df0
Satyam Singh <65668047+satyam4484@users.noreply.github.com>
2026-06-24 08:52:45 +0530

    Time: 0 ms (100%), Space: 17.5 MB (53.45%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
-    int findFirst(vector<int>&nums,int &target) {
-        int low = 0,high = nums.size()-1;
+    int findFirst(vector<int>& nums, int& target, bool reversed = false) {
+        int low = 0, high = nums.size() - 1;
         int ans = -1;
-        while(low <= high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
-            if(target == nums[mid]) {
+            if (target == nums[mid]) {
                 ans = mid;
-                high=mid-1;
-            }else if(target > nums[mid]) {
-                low = mid+1;
-            }else {
-                high = mid-1;
+                if (reversed) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return ans;
     }
-    int findLast(vector<int>&nums,int &target) {
-        int low = 0,high = nums.size()-1;
-        int ans = -1;
-        while(low <= high) {
-            int mid = (low + high) / 2;
-            if(target == nums[mid]) {
-                ans = mid;
-                low=mid+1;
-            }else if(target > nums[mid]) {
-                low = mid+1;
-            }else {
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
-        return {findFirst(nums,target),findLast(nums,target)};
+        return {findFirst(nums, target), findFirst(nums, target, true)};
     }
 };
