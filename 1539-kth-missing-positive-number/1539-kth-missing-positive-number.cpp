e50c1dbc4d93fb5a4b145a5278dbbeba759a6eba
Satyam Singh <65668047+satyam4484@users.noreply.github.com>
2026-07-06 19:32:09 +0530

    Time: 0 ms (100%), Space: 13.2 MB (92.9%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    bool isElementPresent(vector<int>& nums, int ele) {
-        int low = 0, high = nums.size() - 1;
+    int findKthPositive(vector<int>& arr, int k) {
+
+        int low = 0;
+        int high = arr.size() - 1;
 
         while (low <= high) {
+
             int mid = low + (high - low) / 2;
 
-            if (nums[mid] == ele)
-                return true;
-            else if (nums[mid] < ele)
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k)
                 low = mid + 1;
             else
                 high = mid - 1;
         }
 
-        return false;
-    }
-
-    int findKthPositive(vector<int>& arr, int k) {
-
-        int maxEle = arr.back();
-
-        vector<int> missing;
-
-        for (int i = 1; i <= maxEle; i++) {
-            if (!isElementPresent(arr, i))
-                missing.push_back(i);
-        }
-
-        if (missing.size() >= k)
-            return missing[k - 1];
-
-        return maxEle + (k - missing.size());
+        return low + k;
     }
 };
